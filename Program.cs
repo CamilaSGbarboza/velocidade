9c0e410a715f578fd763b167d9a9c14d3487728b
camila <camilasangobar@gmail.com>
2021-03-29 18:28:04 -0300

    velocidade

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace velocidade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+          Console.ForegroundColor = ConsoleColor.DarkMagenta;
+          Console.WriteLine("Velocidade");
+          Console.ResetColor();
+          Console.WriteLine();
+          Console.WriteLine("Distância percorrida(m):");
+          double d = Convert.ToDouble(Console.ReadLine());
+          Console.WriteLine("Tempo gasto (s):");
+          double t = Convert.ToDouble(Console.ReadLine());
+          Console.WriteLine();
+          double v = d / t;
+          Console.WriteLine($"Velocidade média :{v} m/s");
+
+
+
+
+          
+
+          
+        
+        
+
+        }
+    }
+}
